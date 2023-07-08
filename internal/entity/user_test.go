8b9c3e827acbb28016c7176c3c688b86d9ea74ee
César O. Araújo <oaraujocesar@gmail.com>
2023-07-07 22:52:59 -0300

    user entity

---
+++ internal/entity/user_test.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("John Doe", "john@doe.com", "123456")
+	assert.Nil(t, err)
+
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Email)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "john@doe.com", user.Email)
+	assert.Equal(t, "John Doe", user.Name)
+}
+
+func TestValidatePassword(t *testing.T) {
+	user, _ := NewUser("John Doe", "john@doe.com", "123456")
+
+	assert.True(t, user.ValidatePassword("123456"))
+	assert.False(t, user.ValidatePassword("654321"))
+	assert.NotEqual(t, user.Password, "123456")
+}
